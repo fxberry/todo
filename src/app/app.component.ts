ede59fb0959129e58d16d08f3fc1e601916f509a
Felix Egli <f.egli@hsr.ch>
2019-05-20 15:40:06 +0200

    added basic todo app with service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Route } from '@angular/router';
 
+/*
+ * App Component
+ * Top Level Component
+ */
 @Component({
-  selector: 'app-root',
+  selector: 'todo-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo';
+  public name = '2BIT Angular Demos';
+  public navItems: Route[];
+
+  constructor() {
+    // this.navItems = appRoutes.filter((route) => route.data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MatIconModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { SharedModule } from './shared/shared.module';
+import { TodoMvcPlusServiceModule } from './todo/todo.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    TodoMvcPlusServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
